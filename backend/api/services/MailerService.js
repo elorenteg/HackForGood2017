3bf721f3c38e4e76777767d63ebe25c83ce248f6
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-03-11 00:23:32 +0100

    recovered all :_V

--- backend/api/services/MailerService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const mailer = require('sails-service-mailer');
-const config = require('../../config/services/mailer');
-
-module.exports = mailer('sendmail', config.services.mailer);
--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,62 @@
-require('sails').lift(require('rc')('sails'));
+/**
+ * Created by Juan on 10/03/2017.
+ */
+
+var express = require("express"),
+  app = express(),
+  bodyParser  = require("body-parser"),
+  methodOverride = require("method-override"),
+  mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var xml2js = require('xml2js');
+var fs = require('fs');
+var json_preinscripciones = "";
+
+//Load file
+fs.readFile('./data/Prescripcion.xml', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  data;
+  console.log("preinscripciones leidas");
+  //parse file into Json
+  xml2js.parseString(data, function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
+    json_preinscripciones = JSON.stringify(result);
+    console.log("xml parseado en JSON");
+  });
+});
+
+
+
+//get by code function
+function getByCode(code) {
+  console.log(json_preinscripciones);
+}
+
+var router = express.Router();
+
+router.get('/:code', function(req, res) {
+  //var code = req.body.code; //post
+  var code = req.params.code;
+
+
+  res.send("code:" +  code);
+});
+
+router.get('/', function(req, res) {
+  getByCode(66346);
+  res.send("por favor, usa la api bien... MIAU");
+});
+
+app.use(router);
+
+app.listen(80, function() {
+  console.log("Node server running on http://c3cce9a9.ngrok.io/");
+});
