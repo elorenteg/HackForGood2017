646d93195151d42509dd974604e8580fe585e66f
LaQuay <marcvila10@hotmail.com>
2017-03-11 00:25:48 +0100

    First version keyboard fragment

--- android/app/src/main/java/com/hackforgood/dev/hackforgood2017/KeyboardSearchFragment.java
+++ android/app/src/main/java/com/hackforgood/dev/hackforgood2017/KeyboardSearchFragment.java
@@ -1,11 +1,14 @@
 package com.hackforgood.dev.hackforgood2017;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.beardedhen.androidbootstrap.BootstrapEditText;
 
 /**
  * Created by LaQuay on 10/03/2017.
@@ -13,6 +16,9 @@ import android.view.ViewGroup;
 
 public class KeyboardSearchFragment extends Fragment {
     public static final String TAG = KeyboardSearchFragment.class.getSimpleName();
+    private View rootview;
+    private BootstrapButton buttonSearch;
+    private BootstrapEditText editText;
 
     public static KeyboardSearchFragment newInstance() {
         return new KeyboardSearchFragment();
@@ -21,8 +27,30 @@ public class KeyboardSearchFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        rootview = inflater.inflate(R.layout.keyboard_search_fragment, container, false);
 
+        setUpElements();
+        setUpListeners();
 
+        return rootview;
+    }
+
+    private void setUpElements() {
+        editText = (BootstrapEditText) rootview.findViewById(R.id.keyboard_search_edit_text);
+        buttonSearch = (BootstrapButton) rootview.findViewById(R.id.keyboard_search_button);
+    }
+
+    private void setUpListeners() {
+        editText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
