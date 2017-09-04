3c2897011481df5ec1b8631e53d1c857c7ea1bfa
Juan Salmerón Moya <jnssterrass@gmail.com>
2017-09-04 21:01:52 +0200

    port with environment variable

--- backend/app.js
+++ backend/app.js
@@ -170,7 +170,8 @@ router.get('/', function(req, res) {
 });
 
 /**************************************************WEB SERVER**********************************************************/
+var port = process.env.PORT || 80;
 app.use(router);
-app.listen(80, function() {
-  console.log("Node server running on https://hackforgoodbcn2017app.herokuapp.com/");
+app.listen(port, function() {
+  console.log("Node server running on https://hackforgoodbcn2017app.herokuapp.com:"+port+"/");
 });
